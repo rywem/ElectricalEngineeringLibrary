8e91958a35b4db1439833952faae66db0147522f
Ryan <4731255+rywem@users.noreply.github.com>
2025-06-13 05:44:40 -0600

    initial commit

---
+++ ElectricalEngineeringHelper/ElectricalEngineeringHelper/ResistanceHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace ElectricalEngineeringHelper
+{
+    public class ResistanceHelper
+    {
+        public static double CalculateParallelResistance(params double[] resistances)
+        {
+            if (resistances == null || resistances.Length == 0)
+                throw new ArgumentException("At least one resistance value must be provided.");
+            double reciprocalSum = 0.0;
+            foreach (var resistance in resistances)
+            {
+                if (resistance <= 0)
+                    throw new ArgumentException("Resistance values must be greater than zero.");
+                reciprocalSum += 1.0 / resistance;
+            }
+            return 1.0 / reciprocalSum;
+        }
+    }
+}
---
+++ EngineeringConsole/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ElectricalEngineeringHelper;
+
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine(ResistanceHelper.CalculateParallelResistance(2000,1000));
